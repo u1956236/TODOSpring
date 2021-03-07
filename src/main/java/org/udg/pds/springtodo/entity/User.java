d0223b80a4da96c8536b3369dfcc1d338bcc84de
Jordi Gomara Cruz <62349008+u1956236@users.noreply.github.com>
2021-03-07 03:17:55 +0100

    OneToMany created

--- src/main/java/org/udg/pds/springtodo/entity/User.java
+++ src/main/java/org/udg/pds/springtodo/entity/User.java
@@ -1,113 +1,95 @@
 package org.udg.pds.springtodo.entity;
 
-import com.fasterxml.jackson.annotation.*;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonView;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.HashSet;
-import java.util.Set;
 
 @Entity(name = "users")
 @Table(uniqueConstraints = @UniqueConstraint(columnNames = {"email", "username"}))
-@JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "id", scope = User.class)
 public class User implements Serializable {
-    /**
-     * Default value included to remove warning. Remove or modify at will. *
-     */
-    private static final long serialVersionUID = 1L;
+  /**
+   * Default value included to remove warning. Remove or modify at will. *
+   */
+  private static final long serialVersionUID = 1L;
 
-    public User() {
-    }
+  public User() {
+  }
 
-    public User(String username, String email, String password) {
-        this.username = username;
-        this.email = email;
-        this.password = password;
-        this.tasks = new ArrayList<>();
-    }
+  public User(String username, String email, String password) {
+    this.username = username;
+    this.email = email;
+    this.password = password;
+    this.tasks = new ArrayList<>();
+  }
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
 
-    @NotNull
-    private String username;
+  @NotNull
+  private String username;
 
-    @NotNull
-    private String email;
+  @NotNull
+  private String email;
 
-    @NotNull
-    private String password;
+  @NotNull
+  private String password;
 
-    @OneToMany(cascade = CascadeType.ALL, mappedBy = "user")
-    private Collection<Task> tasks;
+  @OneToMany(cascade = CascadeType.ALL, mappedBy = "user")
+  private Collection<Task> tasks;
 
-    // Use Set<> to avoid duplicates. A group cannot be owned more than once
-    @OneToMany(mappedBy = "owner")
-    private Set<Group> ownedGroups = new HashSet<>();
+  @JsonView(Views.Private.class)
+  public Long getId() {
+    return id;
+  }
 
-    // Use Set<> to avoid duplicates. A member cannot be duplicated in a group
-    @ManyToMany(mappedBy = "members", cascade = CascadeType.ALL)
-    private Set<Group> memberGroups = new HashSet<>();
+  public void setId(Long id) {
+    this.id = id;
+  }
 
-    @JsonView(Views.Private.class)
-    public Long getId() {
-        return id;
-    }
+  @JsonView(Views.Private.class)
+  public String getEmail() {
+    return email;
+  }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+  @JsonView(Views.Public.class)
+  public String getUsername() {
+    return username;
+  }
 
-    @JsonView(Views.Private.class)
-    public String getEmail() {
-        return email;
-    }
+  @JsonIgnore
+  public String getPassword() {
+    return password;
+  }
 
-    @JsonView(Views.Public.class)
-    public String getUsername() {
-        return username;
-    }
+  @JsonView(Views.Complete.class)
+  public Collection<Task> getTasks() {
+    // Since tasks is collection controlled by JPA, it has LAZY loading by default. That means
+    // that you have to query the object (calling size(), for example) to get the list initialized
+    // More: http://www.javabeat.net/jpa-lazy-eager-loading/
+    tasks.size();
+    return tasks;
+  }
 
-    @JsonIgnore
-    public String getPassword() {
-        return password;
-    }
+  public void addTask(Task task) {
+    tasks.add(task);
+  }
 
-    @JsonView(Views.Complete.class)
-    public Collection<Task> getTasks() {
-        // Since tasks is collection controlled by JPA, it has LAZY loading by default. That means
-        // that you have to query the object (calling size(), for example) to get the list initialized
-        // More: http://www.javabeat.net/jpa-lazy-eager-loading/
-        tasks.size();
-        return tasks;
-    }
+  @OneToMany(cascade = CascadeType.ALL, mappedBy = "usergroup")
+  	private Collection<Group> groups;
 
-    public void addTask(Task task) {
-        tasks.add(task);
-    }
+	public Collection<users> getGroups() {
+		groups.size();
+		return groups;
+	}
 
-    @JsonView(Views.Private.class)
-    public Collection<Group> getOwnedGroups() {
-        ownedGroups.size();
-        return ownedGroups;
-    }
-
-    public void addOwnedGroup(Group g) {
-        ownedGroups.add(g);
-    }
-
-    @JsonView(Views.Complete.class)
-    public Collection<Group> getMemberGroups() {
-        memberGroups.size();
-        return memberGroups;
-    }
-
-    public void addMemberGroup(Group g) {
-        memberGroups.add(g);
-    }
+	public void addGroups(Groups group) {
+		groups.add(group);
+	}
 
 }
