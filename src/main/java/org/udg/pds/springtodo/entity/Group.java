b72198a5cfea4315866c38afe17a0833e0c05671
Jordi Gomara Cruz <62349008+u1956236@users.noreply.github.com>
2021-03-07 03:20:20 +0100

    Group created

--- src/main/java/org/udg/pds/springtodo/entity/Group.java
+++ src/main/java/org/udg/pds/springtodo/entity/Group.java
@@ -1,86 +1,56 @@
 package org.udg.pds.springtodo.entity;
 
-import com.fasterxml.jackson.annotation.*;
-import com.fasterxml.jackson.databind.annotation.JsonSerialize;
-import org.udg.pds.springtodo.serializer.JsonUserSerializer;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonView;
 
 import javax.persistence.*;
-import javax.validation.constraints.NotNull;
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.HashSet;
-import java.util.Set;
 
-@Entity(name = "groups")
-@JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "id", scope = Group.class)
-public class Group {
-    public Group() {
+@Entity(name="usergroup")
+
+public class Group implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	public Group(){
+	}
+
+	public Group(String name, String description){
+		this.name = name;
+		this.description = description;
+	}
+
+	@NotNull
+	private String name;
+	
+	@NotNull
+	private String description;
+
+	public String getName() {
+		return name;
+	}
+
+	public String getDescription() {
+        return description;
     }
 
-    public Group(String name, String description) {
-        this.name = name;
-        this.description = description;
-    }
+	@ManyToOne(fetch = FetchType.EAGER)
+  	@JoinColumn(name = "fk_user")
+  	private User user;
 
-    // This tells JAXB that this field can be used as ID
-    // Since XmlID can only be used on Strings, we need to use LongAdapter to transform Long <-> String
-    @Id
-    // Don't forget to use the extra argument "strategy = GenerationType.IDENTITY" to get AUTO_INCREMENT
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+ 	@Column(name = "fk_user", insertable = false, updatable = false)
+  	private Long userId;
 
-    @NotNull
-    private String name;
+	@JsonIgnore
+	public User getUser() {
+		return user;
+	}
 
-    private String description;
-
-    @ManyToOne(fetch = FetchType.EAGER)
-    @JoinColumn(name = "fk_owner")
-    private User owner;
-
-    @Column(name = "fk_owner", insertable = false, updatable = false)
-    private Long ownerId;
-
-    @ManyToMany(cascade = CascadeType.MERGE)
-    private Set<User> members = new HashSet<>();
-
-    @JsonView(Views.Private.class)
-    public Long getId() {
-        return id;
-    }
-
-    @JsonIgnore
-    public User getOwner() {
-        return owner;
-    }
-
-    public void setOwner(User owner) {
-        this.owner = owner;
-    }
-
-    public void addMember(User u) {
-        this.members.add(u);
-    }
-
-    @JsonView(Views.Private.class)
-    public Collection<User> getMembers() {
-        members.size();
-        return members;
-    }
-
-    @JsonView(Views.Complete.class)
-    public long getOwnerId() {
-        return ownerId;
-    }
-
-    @JsonView(Views.Private.class)
-    public String getName() {
-        return this.name;
-    }
-
-    @JsonView(Views.Private.class)
-    public String getDescription() {
-        return this.description;
-    }
+	public void setUser(User user) {
+		this.user = user;
+	}
 
 }
+
